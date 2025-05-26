59e2e6e37b6f4dcc5eec69fd07a2721b2f088202
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-05-26 19:02:02 +0100

    Update bootstrap.php

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR die;
  * CI Skeleton Application version.
  * @since 	2.145
  */
-const APP_VERSION = '1.1';
+const APP_VERSION = '1.2';
 
 /*
 |--------------------------------------------------------------------------
