54992a2f24d3b0ee2679c134b258af47ce611aff
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-06-02 01:16:20 +0100

    feat!: added `admin_app_lang.php` file

---
+++ application/language/brazilian/admin_app_lang.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR die;
+
+/**
+ * Dashboard Language (Brazilian Portuguese)
+ *
+ * @package 	CodeIgniter
+ * @subpackage 	Skeleton\Application
+ * @category 	Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @link        http://bit.ly/KaderGhb
+ * @copyright   Copyright (c) 2025, Kader Bouyakoub (http://bit.ly/KaderGhb)
+ */
