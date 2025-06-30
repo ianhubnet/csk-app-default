79bd79d577f0d0c04b5b29ba0d92e17df5668048
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-06-30 03:42:11 +0100

    refactor: removed `production/config.php`

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,16 +1,4 @@
 <?php
 defined('BASEPATH') || exit('A moment of silence for your attempt.');
 
-/*
-|--------------------------------------------------------------------------
-| Static Content (CDN) URL.
-|--------------------------------------------------------------------------
-|
-| This URL is used to serve static assets such as images, stylesheets, and scripts.
-| It should point to a subdomain or external service (e.g., CDN) that handles
-| static content delivery. When defined, asset URLs throughout the system will be
-| prefixed with this value to improve load speed and caching.
-|
-| Example: //cdn.example.com/
-*/
-// $config['static_url'] = 'https://cdn.example.com/';
+
