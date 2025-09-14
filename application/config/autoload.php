64015b9d0e68d7832fc5330e07050971c05a49f4
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-09-14 02:57:14 +0100

    refactor: Refactored `config/autoload.php`

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,11 +4,7 @@
  * Auto-loader
  *
  * This file specifies which components should be automatically loaded
- * with every request. It supports both:
- *
- *  1. `$autoload` - Items loaded early in the request lifecycle.
- *  2. `$deferred` - Items loaded later, when the "post_loader" hook
- *     is triggered manually via `do_action('post_loader')`.
+ * with every request. It supports a single `$autoload` array.
  *
  * You can autoload the following components:
  *
@@ -17,11 +13,13 @@
  *   - drivers   : Array of drivers (optionally aliased).
  *   - helper    : Array of helper files.
  *   - config    : Array of custom config files.
- *   - language  : Array of language files (better use `$deferred` for them).
+ *   - language  : Array of language files (these are always deferred
+ *                 until the active language has been determined).
  *   - model     : Array of models.
  *
- * NOTE: Language files should generally go in `$deferred` to ensure
- * that locale/theme-dependent context is available when loading them.
+ * NOTE: Language files are *always* loaded later in the process,
+ * after the application locale has been set. No special handling
+ * is required from you.
  *
  * @package    App\Config
  * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
@@ -33,10 +31,8 @@
 | -------------------------------------------------------------------
 |  Auto-load Packages
 | -------------------------------------------------------------------
-| Prototype:
-|
+| Example:
 |  $autoload['packages'] = [APPPATH.'third_party', '/usr/local/shared'];
-|
 */
 $autoload['packages'] = [];
 
@@ -44,17 +40,10 @@ $autoload['packages'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Libraries
 | -------------------------------------------------------------------
-| These are the classes located in system/libraries/ or your
-| application/libraries/ directory.
-|
-| Prototype:
-|
-|	$autoload['libraries'] = ['email', 'session'];
-|
-| You can also supply an alternative library name to be assigned
-| in the controller:
-|
-|	$autoload['libraries'] = ['user_agent' => 'ua'];
+| Example:
+|  $autoload['libraries'] = ['email', 'session'];
+| You can also alias:
+|  $autoload['libraries'] = ['agent' => 'ua'];
 */
 $autoload['libraries'] = [];
 
@@ -62,20 +51,10 @@ $autoload['libraries'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Drivers
 | -------------------------------------------------------------------
-| These classes are located in system/libraries/ or in your
-| application/libraries/ directory, but are also placed inside their
-| own subdirectory and they extend the CI_Driver_Library class. They
-| offer multiple interchangeable driver options.
-|
-| Prototype:
-|
-|	$autoload['drivers'] = ['cache'];
-|
-| You can also supply an alternative property name to be assigned in
-| the controller:
-|
-|	$autoload['drivers'] = ['cache' => 'cch'];
-|
+| Example:
+|  $autoload['drivers'] = ['cache'];
+| You can also alias:
+|  $autoload['drivers'] = ['cache' => 'cch'];
 */
 $autoload['drivers'] = [];
 
@@ -83,9 +62,8 @@ $autoload['drivers'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Helper Files
 | -------------------------------------------------------------------
-| Prototype:
-|
-|	$autoload['helper'] = ['url', 'file'];
+| Example:
+|  $autoload['helper'] = ['url', 'file'];
 */
 $autoload['helper'] = [];
 
@@ -93,13 +71,9 @@ $autoload['helper'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Config files
 | -------------------------------------------------------------------
-| Prototype:
-|
-|	$autoload['config'] = ['config1', 'config2'];
-|
-| NOTE: This item is intended for use ONLY if you have created custom
-| config files.  Otherwise, leave it blank.
-|
+| Example:
+|  $autoload['config'] = ['config1', 'config2'];
+| NOTE: Only for custom config files.
 */
 $autoload['config'] = [];
 
@@ -107,13 +81,14 @@ $autoload['config'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Language files
 | -------------------------------------------------------------------
-| Prototype:
+| Example:
+|  $autoload['language'] = ['core', 'app'];
 |
-|	$autoload['language'] = ['lang1', 'lang2'];
+| NOTE: Do not include the "_lang" suffix.
+| E.g. "core_lang.php" → ['core']
 |
-| NOTE: Do not include the "_lang" part of your file.  For example
-| "codeigniter_lang.php" would be referenced as ['codeigniter'];
-|
+| These are automatically deferred until the active language
+| has been determined.
 */
 $autoload['language'] = [];
 
@@ -121,13 +96,9 @@ $autoload['language'] = [];
 | -------------------------------------------------------------------
 |  Auto-load Models
 | -------------------------------------------------------------------
-| Prototype:
-|
-|	$autoload['model'] = ['first_model', 'second_model'];
-|
-| You can also supply an alternative model name to be assigned
-| in the controller:
-|
-|	$autoload['model'] = ['first_model' => 'first'];
+| Example:
+|  $autoload['model'] = ['first_model', 'second_model'];
+| You can also alias:
+|  $autoload['model'] = ['first_model' => 'first'];
 */
 $autoload['model'] = [];
