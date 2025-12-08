5af60d7e8bb18a7bd69068057e68a13ab4daf4e6
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-12-08 03:46:46 +0100

    chore(lang): Added AI generated Croatian and Slovak

---
+++ application/language/croatian/app_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * App Language File (Croatian)
+ *
+ * Contains all translatable strings for the application.
+ *
+ * Notes:
+ * - Site name, description, keywords, and author use the `config:site_*` keys.
+ * - Other strings (e.g., welcome messages, labels) are grouped by context.
+ * - You can override any string in the file to customize translations.
+ *
+ * @package    App\Language
+ * @category   App
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2025, Kader Bouyakoub
+ * @since      1.0
+ */
+
+// --------------------------------------------------------------------
+// Translatable: Site name, description, keywords, and author
+// --------------------------------------------------------------------
+// $lang['config:site_name'] = Platform::LABEL;
+// $lang['config:site_description'] = Platform::SLOGAN;
+// $lang['config:site_keywords'] = Platform::KEYWORDS;
+// $lang['config:site_author'] = Platform::AUTHOR;
+
+// --------------------------------------------------------------------
+
+$lang['welcome_h1'] = 'Dobrodošli u CiSkeleton!';
+$lang['welcome_p1'] = 'Stranicu koju gledate dinamički generira CiSkeleton.';
+$lang['welcome_p2'] = 'Ako želite urediti ovu stranicu, naći ćete je na:';
+$lang['welcome_p3'] = 'Odgovarajući kontroler za ovu stranicu nalazi se na:';
+$lang['welcome_p4'] = 'Ako prvi put istražujete CiSkeleton, trebali biste početi čitanjem <a href="%s" target="_blank">Korisničkog priručnika</a>.';
+$lang['welcome_footer'] = 'Stranica je generirana za <strong>%s</strong> sekundi. CodeIgniter <strong>v%s</strong>. Skeleton <strong>v%s</strong>.';
---
+++ application/language/slovak/admin/app_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Dashboard Language File (Slovak)
+ *
+ * Contains all translatable strings for the admin interface.
+ *
+ * Note: Strings should be grouped by context for easy maintenance.
+ *
+ * @package    App\Language
+ * @category   Admin
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2025, Kader Bouyakoub
+ * @since      1.0
+ */
+
+$lang['menu_main'] = 'Hlavné menu';
+$lang['menu_social'] = 'Sociálne menu';
