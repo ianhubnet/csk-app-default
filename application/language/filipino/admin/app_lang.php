f776aa7bdbb80ea351a31101cd47e22dae665927
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-01-04 03:59:36 +0100

    chore(lang): Added AI generated Filipino

---
+++ application/language/filipino/admin/app_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Dashboard Language File (Filipino)
+ *
+ * Contains all translatable strings for the admin interface.
+ *
+ * Note: Strings should be grouped by context for easy maintenance.
+ *
+ * @package    App\Language
+ * @category   Admin
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2025, Kader Bouyakoub
+ * @since      1.0
+ */
+
+$lang['menu_main'] = 'Pangunahing Menu';
+$lang['menu_social'] = 'Menu ng Social Media';
---
+++ application/language/filipino/app_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * App Language File (Filipino)
+ *
+ * Contains all translatable strings for the application.
+ *
+ * Notes:
+ * - Site name, description, keywords, and author use the `config:site_*` keys.
+ * - Other strings (e.g., welcome messages, labels) are grouped by context.
+ * - You can override any string in the file to customize translations.
+ *
+ * @package    App\Language
+ * @category   App
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2025, Kader Bouyakoub
+ * @since      1.0
+ */
+
+// --------------------------------------------------------------------
+// Translatable: Site name, description, keywords, and author
+// --------------------------------------------------------------------
+// $lang['config:site_name'] = Platform::LABEL;
+// $lang['config:site_description'] = Platform::SLOGAN;
+// $lang['config:site_keywords'] = Platform::KEYWORDS;
+// $lang['config:site_author'] = Platform::AUTHOR;
+
+// --------------------------------------------------------------------
+
+$lang['welcome_h1'] = 'Maligayang pagdating sa CiSkeleton!';
+$lang['welcome_p1'] = 'Ang pahinang iyong tinitingnan ay dynamic na binubuo ng CiSkeleton.';
+$lang['welcome_p2'] = 'Kung nais mong baguhin ang pahinang ito, makikita mo ito sa:';
+$lang['welcome_p3'] = 'Ang katumbas na controller para sa pahinang ito ay matatagpuan sa:';
+$lang['welcome_p4'] = 'Kung unang beses mo lang susuriin ang CiSkeleton, dapat mong simulan sa pamamagitan ng pagbabasa ng <a href="%s" target="_blank">Gabay sa Gumagamit</a>.';
+$lang['welcome_footer'] = 'Na-render ang pahina sa loob ng <strong>%s</strong>&nbsp;segundo. CodeIgniter <strong>v%s</strong>. CiSkeleton <strong>v%s</strong>.';
