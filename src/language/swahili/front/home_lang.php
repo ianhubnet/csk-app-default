1dc8d77de43d61cba2c6218a1ade6e4940ba7363
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-18 00:55:46 +0100

    feat(lang): add AI generate Swahili

---
+++ src/language/swahili/front/home_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Front Language File - Homepage (Swahili)
+ *
+ * @package    App\Language
+ * @category   Front
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2026, Kader Bouyakoub
+ * @since      0.0.1
+ */
+
+$lang['front_home_h1'] = 'Karibu {site_name}!';
+$lang['front_home_p1'] = 'Ukurasa unaotazama unatengenezwa kiotomatiki na {platform_name}.';
+$lang['front_home_p2'] = 'Ikiwa ungependa kuhariri ukurasa huu utaupata kwenye:';
+$lang['front_home_p3'] = 'Kidhibiti kinachohusiana na ukurasa huu kinapatikana kwenye:';
+$lang['front_home_p4'] = 'Ikiwa unachunguza %s kwa mara ya kwanza kabisa, unapaswa kuanza kwa kusoma <a href="%s" target="_blank">Mwongozo wa Mtumiaji</a>.';
+$lang['front_home_footer'] = 'Ukurasa umetolewa ndani ya sekunde <strong>%s</strong>.';
