e187416ad1ed8b13123220221b42e6b1f165965f
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-19 23:02:42 +0100

    feat(lang): add AI generate Estonian translation

---
+++ src/language/estonian/admin/app_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Dashboard Language File (Estonian)
+ *
+ * Contains all translatable strings for the admin interface.
+ *
+ * Note: Strings should be grouped by context for easy maintenance.
+ *
+ * @package    App\Language
+ * @category   Admin
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2026, Kader Bouyakoub
+ * @since      0.0.1
+ */
+
+$lang['menu_main'] = 'Peamenüü';
+$lang['menu_social'] = 'Sotsiaalmeedia menüü';
---
+++ src/language/estonian/front/home_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Front Language File - Homepage (Estonian)
+ *
+ * @package    App\Language
+ * @category   Front
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2026, Kader Bouyakoub
+ * @since      0.0.1
+ */
+
+$lang['front_home_h1'] = 'Tere tulemast {site_name}!';
+$lang['front_home_p1'] = 'Leht, mida te praegu vaatate, genereeritakse dünaamiliselt {platform_name} poolt.';
+$lang['front_home_p2'] = 'Kui soovite seda lehekülge muuta, leiate selle aadressilt:';
+$lang['front_home_p3'] = 'Selle lehekülje vastav kontroller asub aadressil:';
+$lang['front_home_p4'] = 'Kui külastate %s esimest korda, peaksite alustama <a href="%s" target="_blank">kasutusjuhendi</a> lugemisest.';
+$lang['front_home_footer'] = 'Lehekülg renderdati <strong>%s</strong> sekundiga.';
