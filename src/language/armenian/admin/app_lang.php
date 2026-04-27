d7346154816f9665566519b900f23c5da9771746
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-27 03:47:02 +0100

    feat(lang): add Google-Translated "armenian"

---
+++ src/language/armenian/admin/app_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Dashboard Language File (Armenian)
+ *
+ * Contains all translatable strings for the admin interface.
+ *
+ * Note: Strings should be grouped by context for easy maintenance.
+ *
+ * @package    App\Language
+ * @category   Admin
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2026, Kader Bouyakoub
+ * @since      0.0.1
+ */
+
+$lang['menu_main'] = 'Գլխավոր մենյու';
+$lang['menu_social'] = 'Սոցիալական մենյու';
---
+++ src/language/armenian/front/home_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Front Language File - Homepage (Armenian)
+ *
+ * @package    App\Language
+ * @category   Front
+ * @author     Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright  Copyright (c) 2026, Kader Bouyakoub
+ * @since      0.0.1
+ */
+
+$lang['front_home_h1'] = 'Բարի գալուստ {site_name}!';
+$lang['front_home_p1'] = 'Էջը, որը դուք դիտում եք, դինամիկ կերպով գեներացվում է {platform_name}-ի կողմից։';
+$lang['front_home_p2'] = 'Եթե ցանկանում եք խմբագրել այս էջը, այն գտնվում է հետևյալ հասցեում՝';
+$lang['front_home_p3'] = 'Այս էջի համար համապատասխան կոնտրոլլերը գտնվում է՝';
+$lang['front_home_p4'] = 'Եթե դուք առաջին անգամ եք ուսումնասիրում %s-ը, սկսեք՝ կարդալով <a href="%s" target="_blank">Օգտվողի ուղեցույցը</a>։';
+$lang['front_home_footer'] = 'Էջը մշակվել է <strong>%s</strong> վայրկյանում։';
